5d412a46c5538534c5131bfae5afe27d0dd256cd
KuangjuX <qcx@tju.edu.cn>
2021-03-10 07:48:37 -0800

    add kvmmake, kvmmap

--- user/target/debug/deps/user-36188430df376565.d
+++
@@ -1,5 +0,0 @@
-/home/kuangjux/xv6-rust/user/target/debug/deps/user-36188430df376565.rmeta: src/main.rs
-
-/home/kuangjux/xv6-rust/user/target/debug/deps/user-36188430df376565.d: src/main.rs
-
-src/main.rs:
