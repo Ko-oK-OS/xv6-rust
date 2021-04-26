fe56d3a167f19a1902520e0ad269f17b3c5a6976
KuangjuX <qcx@tju.edu.cn>
2021-04-26 06:16:32 -0700

    impl sbrk

--- kernel/src/syscall/sysproc.rs
+++ kernel/src/syscall/sysproc.rs
@@ -1,4 +1,5 @@
 use crate::process::*;
+use super::*;
 
 pub fn sys_fork() -> isize {
     unsafe{
@@ -9,3 +10,24 @@ pub fn sys_fork() -> isize {
 // pub fn sys_exit() -> isize {
 
 // }
+
+pub fn sys_sbrk() -> usize {
+    let mut size: usize = 0;
+
+    // get syscall argument
+    argint(0, &mut size);
+
+    let my_proc = unsafe{ CPU_MANAGER.myproc().unwrap() };
+    let addr = my_proc.extern_data.get_mut().size;
+    match my_proc.growproc(size as isize) {
+        Ok(()) => {
+            return addr
+        }
+
+        Err(err) => {
+            panic!("err: {:?}", err);
+        }
+    }
+
+    
+}
